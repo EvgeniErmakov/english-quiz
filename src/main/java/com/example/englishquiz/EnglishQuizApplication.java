79faec11e10d1a755d27b78364fd1a7ccb55d09a
Evgeni Ermakov <evgeniermakov1994@gmail.com>
2026-05-24 14:09:28 +0300

    Remove binary Gradle wrapper jar from repository

---
+++ src/main/java/com/example/englishquiz/EnglishQuizApplication.java
@@ -0,0 +1,12 @@
+package com.example.englishquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishQuizApplication.class, args);
+    }
+}
